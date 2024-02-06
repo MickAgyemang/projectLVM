1c173ad41e5f033fe7026725ae610bc517b0f60a
Udesdylan <177594@student.aventus.nl>
2024-02-06 16:34:14 +0100

    update

---
+++ resources/views/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="recources/app.css">
+    <title>home page</title>
+</head>
+<header>
+<ul>
+  <li><a href="index.php">Home</a></li>
+  <li><a href="index.php">News</a></li>
+  <li><a href="index.php">Contact</a></li>
+  <li><a href="index.php">About</a></li>
+</ul>
+</header>
+<body>
+    
+</body>
+</html>
